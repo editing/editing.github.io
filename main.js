f0740593cb9cb9d1b61feac34720bba2c1b29fa7
Thaina <thainayu@gmail.com>
2016-07-24 02:44:12 +0700

    no require

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
 /* global $,angular,require,monaco */
 
-require.config({ paths: { 'vs': 'monaco-editor/min/vs' } });
-require(['vs/editor/editor.main'], function () {
+var module = angular.module("app", ["ngMaterial"]);
 
-	angular.module("app", ["ngMaterial"]).controller("body", ($scope) => {
-		function Cloudant(method, path) {
-			return $.ajax({
-				method: method,
-				url: "https://" + $scope.user + ":" + $scope.pass + "@" + $scope.domain + ".cloudant.com" + path
+module.controller("body", ($scope) => {
+	function Cloudant(method, path) {
+		return $.ajax({
+			method: method,
+			url: "https://" + $scope.user + ":" + $scope.pass + "@" + $scope.domain + ".cloudant.com" + path
+		});
+	}
+
+	$scope.login = function () {
+		Cloudant("GET", "/_users/_all_docs?startkey='_design/'&endkey='_design/\uFFFF'").then((data, status) => {
+			$scope.status = status;
+			$scope.$apply();
+
+			var editor = monaco.editor.create(document.getElementById('container'), {
+				language: 'json',
+				value: data
 			});
-		}
-
-		$scope.login = function () {
-			Cloudant("GET", "/_users/_all_docs?startkey='_design/'&endkey='_design/\uFFFF'").then((data, status) => {
-				$scope.status = status;
-				$scope.$apply();
-
-				var editor = monaco.editor.create(document.getElementById('container'), {
-					language: 'json',
-					value: data
-				});
-			}, (req, status, err) => {
-				console.error(err);
-				$scope.status = status;
-				$scope.$apply();
-			});
-		};
-	});
-
+		}, (req, status, err) => {
+			console.error(err);
+			$scope.status = status;
+			$scope.$apply();
+		});
+	};
 });
