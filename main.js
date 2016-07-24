1cecf9f6c607359e45c3c919e265421a13e62bcb
Thaina <thainayu@gmail.com>
2016-07-25 00:47:38 +0700

    JSON.stringify

--- main.js
+++ main.js
@@ -11,17 +11,16 @@ function Body($scope) {
 	/** @type {monaco.editor.IStandaloneCodeEditor} */
 	var editor;
 	$scope.login = function () {
-		var provider = new firebase.auth.GithubAuthProvider();
-		firebase.auth().signInWithPopup(provider).then((result) => {
-			$scope.accessToken = result.credential.accessToken;
-			return PromiseMonaco;
-		}, (err) => console.error(err)).then(() => {
+		PromiseMonaco.then(() => {
+			var provider = new firebase.auth.GithubAuthProvider();
+			return firebase.auth().signInWithPopup(provider);
+		}, (err) => console.error(err)).then((result) => {
 			if (!editor) {
 				editor = monaco.editor.create(document.getElementById('container'), { language: 'json' });
 				editor.getModel().detectIndentation(false, 4);
 			}
 
-			editor.setValue($scope.accessToken);
+			editor.setValue(JSON.stringify(result,null,"\t"));
 		});
 	};
 }
