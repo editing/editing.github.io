699b1800173b372d7379b05b061c7290b4ba14a9
Thaina <thainayu@gmail.com>
2016-07-25 00:24:42 +0700

    github firebase

--- main.js
+++ main.js
@@ -1,44 +1,27 @@
-/* global $,monaco:monaco,require:require */
+/* global monaco:monaco,require:require,firebase:firebase */
 
 require.config({ paths: { 'vs': 'node_modules/monaco-editor/min/vs' } });
 var PromiseMonaco = new Promise((resolve) => {
-	require(['vs/editor/editor.main'], function () {
-		resolve();
-	});
+	require(['vs/editor/editor.main'], () => resolve());
 });
 
 /* exported */
 function Body($scope) {
-	function Cloudant(method, path) {
-		return $.get({
-			method: method,
-			headers: { Authorization: "Basic " + btoa($scope.user + ":" + $scope.pass) },
-			url: "https://" + $scope.domain + ".cloudant.com" + encodeURI(path)
-		});
-	}
 
 	/** @type {monaco.editor.IStandaloneCodeEditor} */
 	var editor;
 	$scope.login = function () {
-		Cloudant("GET", "/_users/_all_docs?startkey=\"_design/\"&endkey=\"_design/\uFFFF\"").then((data, status) => {
-			$scope.status = status;
-			$scope.$apply();
-
-			return data;
-		}, (req, status, err) => {
-			console.error(err);
-			$scope.status = status;
-			$scope.$apply();
-		}).then((data) => {
+		var provider = new firebase.auth.GithubAuthProvider();
+		firebase.auth().signInWithPopup(provider).then((result) => {
+			$scope.accessToken = result.credential.accessToken;
+			return PromiseMonaco;
+		}, (err) => console.error(err)).then(() => {
 			if (!editor) {
 				editor = monaco.editor.create(document.getElementById('container'), { language: 'json' });
-				editor.getModel().detectIndentation(false,4);
+				editor.getModel().detectIndentation(false, 4);
 			}
 
-			return PromiseMonaco.then(() => data);
-		}, (err) => console.error(err)).then((data) => {
-
-			editor.setValue(JSON.stringify(data));
+			editor.setValue($scope.accessToken);
 		});
 	};
 }
