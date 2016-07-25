04347624d743f32c40e6948e93fd5319aca63e40
Thaina Yu <thaina_greenwolf@hotmail.com>
2016-07-25 10:19:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function Body($scope) {
 	var provider = new firebase.auth.GithubAuthProvider();
 	firebase.auth().getRedirectResult().then((credential) => {
 		if(!credential || !credential.user)
-			firebase.auth().signInWithRedirect(provider);
+			throw firebase.auth().signInWithRedirect(provider);
 		
 		return PromiseMonaco.then(() => credential);
 	},(error) => {
