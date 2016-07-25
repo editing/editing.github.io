685a30bac9ec3b8b67a584c8af9018d6ef89a6f7
Thaina Yu <thaina_greenwolf@hotmail.com>
2016-07-25 10:16:46 +0700

    try getRedirectResult

--- main.js
+++ main.js
@@ -5,22 +5,27 @@ var PromiseMonaco = new Promise((resolve) => {
 	require(['vs/editor/editor.main'], () => resolve());
 });
 
+var editor;
+
 /* exported */
 function Body($scope) {
 
 	/** @type {monaco.editor.IStandaloneCodeEditor} */
-	var editor;
-	$scope.login = function () {
-		var provider = new firebase.auth.GithubAuthProvider();
-		firebase.auth().signInWithRedirect(provider).then((result) => {
-			return PromiseMonaco.then(() => result);
-		}, (err) => console.error(err)).then((result) => {
-			if (!editor) {
-				editor = monaco.editor.create(document.getElementById('container'), { language: 'json' });
-				editor.getModel().detectIndentation(false, 4);
-			}
+	var provider = new firebase.auth.GithubAuthProvider();
+	firebase.auth().getRedirectResult().then((credential) => {
+		if(!credential || !credential.user)
+			signInWithRedirect(provider);
+		
+		return PromiseMonaco.then(() => credential);
+	},(error) => {
+		console.error(error);
+		signInWithRedirect(provider);
+	}).then((result) => {
+		if (!editor) {
+			editor = monaco.editor.create(document.getElementById('container'), { language: 'json' });
+			editor.getModel().detectIndentation(false, 4);
+		}
 
-			editor.setValue(JSON.stringify(result,null,"\t"));
-		});
-	};
+		editor.setValue(JSON.stringify(result,null,"\t"));
+	});
 }
